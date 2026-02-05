fb7fb38ed3a7e40e9285ad874b8283b2114ec412
Mudavath-Giri-Naik <mudavathgirinaik444@gmail.com>
2026-02-05 13:36:52 +0530

    Initial commit

---
+++ components/sections/editor-page-header.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+import type { User } from "@supabase/supabase-js";
+
+import {
+  CreditCardIcon,
+  HelpCircleIcon,
+  HomeIcon,
+  LogOutIcon,
+  MailIcon,
+  Moon,
+  Sun,
+} from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { createClient } from "@/lib/supabase/client";
+
+type EditorPageHeaderProps = {
+  user: User;
+};
+
+export function EditorPageHeader({ user }: EditorPageHeaderProps) {
+  const router = useRouter();
+  const client = createClient();
+  const { setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
+      <div className="container mx-auto max-w-7xl px-4">
+        <div className="flex h-14 items-center justify-between">
+          {/* Logo */}
+          <Link href="/editor" className="flex items-center space-x-2">
+            <div className="text-xl font-bold tracking-tight">Doxy</div>
+          </Link>
+
+          <div className="flex gap-2 items-center">
+            {mounted ? (
+              <>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon-sm">
+                      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                      <span className="sr-only">Toggle theme</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                      Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                      Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                      System
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+
+                {/* User Menu */}
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      variant="ghost"
+                      className="relative h-8 w-8 rounded-full"
+                    >
+                      <Avatar>
+                        <AvatarImage
+                          src={user.user_metadata.avatar_url}
+                          alt="@user"
+                        />
+                        <AvatarFallback>
+                          {user.email?.slice(0, 2).toUpperCase()}
+                        </AvatarFallback>
+                      </Avatar>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent
+                    className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                    side="bottom"
+                    align="end"
+                    sideOffset={4}
+                  >
+                    <DropdownMenuLabel className="p-0 font-normal">
+                      <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                        <Avatar className="h-8 w-8 rounded-lg">
+                          <AvatarImage
+                            src={user.user_metadata.avatar_url}
+                            alt={user.user_metadata.full_name}
+                          />
+                          <AvatarFallback className="rounded-lg">
+                            {user.user_metadata.full_name
+                              ?.slice(0, 2)
+                              .toUpperCase()}
+                          </AvatarFallback>
+                        </Avatar>
+                        <div className="grid flex-1 text-left text-sm leading-tight">
+                          <span className="truncate font-medium">
+                            {user.user_metadata.full_name}
+                          </span>
+                          <span className="text-muted-foreground truncate text-xs">
+                            {user.email}
+                          </span>
+                        </div>
+                      </div>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem asChild>
+                        <Link href="/editor">
+                          <HomeIcon />
+                          Dashboard
+                        </Link>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem disabled>
+                        <CreditCardIcon />
+                        Billing
+                      </DropdownMenuItem>
+                      <DropdownMenuItem asChild>
+                        <a href="https://github.com/htmujahid/undocx">
+                          <HelpCircleIcon />
+                          Support
+                        </a>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem asChild>
+                        <a href="mailto:htmujahid@gmail.com?subject=Doxy Feedback">
+                          <MailIcon />
+                          Send Feedback
+                        </a>
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                      onClick={() => {
+                        client.auth.signOut();
+                        router.refresh();
+                      }}
+                    >
+                      <LogOutIcon />
+                      Log out
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </>
+            ) : (
+              <div className="flex items-center gap-2">
+                <Button variant="outline" size="icon-sm" disabled>
+                  <Sun className="h-[1.2rem] w-[1.2rem]" />
+                </Button>
+                <Button variant="ghost" className="h-8 w-8 rounded-full" disabled />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
